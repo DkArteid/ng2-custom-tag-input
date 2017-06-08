b67612a12ccd4371ae784ae9933bd6d0e19ccabf
Eduardo Muraoka <eduardomuraoka@Eduardos-MacBook-Pro.local>
2017-06-08 00:45:51 -0700

    Changing version and fixing project url on package.json

---
+++ ng2-custom-tag-complete/ng2-custom-tag-complete.component.js
@@ -0,0 +1,12 @@
+class Ng2CustomTagCompleteController {
+    constructor() {
+    }
+}
+
+export default {
+    controller: Ng2CustomTagCompleteController,
+    template: require('./ng2-custom-tag-complete.html'),
+    bindings: {
+        
+    }
+};
